b302d6056bd744f8e0a4f59ca013d61446403836
Delan Azabani <delan@azabani.com>
2026-02-28 12:35:44 +0800

    Changes needed for report on 2025-09-09

--- chromedriver.nix
+++ chromedriver.nix
@@ -10,17 +10,17 @@
 
 stdenv.mkDerivation rec {
   pname = "chromedriver";
-  version = "138.0.7204.168";
+  version = "139.0.7258.154";
 
   src = let
     json = builtins.fromJSON (builtins.readFile (fetchurl {
       url = "https://googlechromelabs.github.io/chrome-for-testing/${version}.json";
-      hash = "sha256-47Ig+EDxFijHLEoMHWFbAWb1xdGliRedBy0IuL+POjo=";
+      hash = "sha256-FO5csnDLVKTRj+YmkWErqzRTTZtnRoXw8CmmiWtXR5k=";
     }));
     url = (lib.findFirst (d: d.platform == "linux64") null json.downloads.chromedriver).url;
   in fetchurl {
     url = url;
-    hash = "sha256-PjU55ZNAfc/VYl+PA4Tvyr0MNHDU1ZDBnWoDOWlGO5A=";
+    hash = "sha256-C445JMIIfL6U57rSfnCEq5CK61NEGk5afx8BwLgqh14=";
   };
 
   nativeBuildInputs = [
